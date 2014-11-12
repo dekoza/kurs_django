3694ca19bc11ffe63052b959a13a0117300bc63f
Dominik Kozaczko <dominik@kozaczko.info>
2014-11-12 18:52:21 +0100

    poprawka: namespace zamiast name!

--- biblio/urls.py
+++ biblio/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^shelf/', include('shelf.urls', name='shelf') ),
+    url(r'^shelf/', include('shelf.urls', namespace='shelf') ),
 )
